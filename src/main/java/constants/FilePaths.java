2c2fc0afac53ea101457be0c98478a1fcfc764a9
apreeti-tw <apreeti@thoughtworks.com>
2021-08-24 23:20:26 +0530

    Refactor PropertiesUtils file

--- src/main/java/constants/FilePaths.java
+++ src/main/java/constants/FilePaths.java
@@ -1,12 +1,13 @@
 package constants;
 
-import lombok.Data;
-
-@Data
 public final class FilePaths {
     private FilePaths(){}
 
     private static final String PATH_TO_RESOURCES = System.getProperty("user.dir")+"/src/main/resources/";
     private static final String PROPERTIES_FILE_PATH = PATH_TO_RESOURCES.concat("application_test.properties");
-    private static final String USERs_EXCEL_FILE_PATH = PATH_TO_RESOURCES.concat("UserDetails.json");
+    private static final String USERS_EXCEL_FILE_PATH = PATH_TO_RESOURCES.concat("UserDetails.json");
+
+    public static String getPropertiesFilePath(){
+        return PROPERTIES_FILE_PATH;
+    }
 }
--- src/main/java/utils/PropertiesUtils.java
+++ src/main/java/utils/PropertiesUtils.java
@@ -1,15 +1,32 @@
 package utils;
 
+import constants.FilePaths;
+
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 public final class PropertiesUtils {
 
     private PropertiesUtils(){}
 
-    public static String getProperty(String property) throws IOException {
-        java.util.Properties properties = new java.util.Properties();
-        properties.load(new FileInputStream(System.getProperty("user.dir") + "/src/main/resources/application_test.properties"));
-        return properties.getProperty(property);
+    private static Properties properties = new Properties();
+    private static Map<String,String> CONFIG_MAP = new HashMap();
+
+    static {
+        try(FileInputStream fis = new FileInputStream(FilePaths.getPropertiesFilePath())) {
+            properties.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        properties.forEach((key, value) -> CONFIG_MAP.put(String.valueOf(key), String.valueOf(value)));
+    }
+
+    public static String getProperty(String key){
+        return CONFIG_MAP.get(key);
     }
 }
