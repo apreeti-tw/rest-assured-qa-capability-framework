9ad14db464340be51e7c6a75cd06e20da1a6c5ae
apreeti-tw <apreeti@thoughtworks.com>
2021-09-02 14:25:04 +0530

    Separate out header and params initialisation to RequestSpecBuilder

--- src/main/java/specifications/RequestSpecBuilder.java
+++ src/main/java/specifications/RequestSpecBuilder.java
@@ -3,7 +3,10 @@ package specifications;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import utils.AuthorizationUtils;
 import utils.PropertiesUtils;
+
+import java.util.Map;
 
 public class RequestSpecBuilder {
 
@@ -19,9 +22,20 @@ public class RequestSpecBuilder {
         return baseRequestSpecification;
     }
 
-    public static RequestSpecification getPathParams(RequestSpecification reqSpec, String[] pathParams) {
-        for (String param:pathParams) {
-            reqSpec = reqSpec.pathParams(param.split(":")[0], param.split(":")[1]);
+    public static RequestSpecification getAuth(RequestSpecification reqSpec, Map<String, String> parameters){
+        if (parameters.get("setAuth").equalsIgnoreCase("Y")) {
+            if (parameters.get("authType").equalsIgnoreCase("oauth2CC")) {
+                reqSpec = reqSpec.auth().oauth2(AuthorizationUtils.getAuthToken(parameters.get("authParams").split(";")));
+            }
+        }
+        return reqSpec;
+    }
+
+    public static RequestSpecification getPathParams(RequestSpecification reqSpec, Map<String, String> parameters) {
+        if (parameters.get("setPathParams").equalsIgnoreCase("Y")) {
+            for (String param:parameters.get("pathParams").split(";")) {
+                reqSpec = reqSpec.pathParams(param.split(":")[0], param.split(":")[1]);
+            }
         }
         return reqSpec;
     }
--- src/test/java/base/BarnBaseTest.java
+++ src/test/java/base/BarnBaseTest.java
@@ -3,22 +3,18 @@ package base;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeMethod;
 import specifications.RequestSpecBuilder;
-import utils.AuthorizationUtils;
 
 import java.util.Map;
+
+import static specifications.RequestSpecBuilder.getAuth;
+import static specifications.RequestSpecBuilder.getPathParams;
 
 public class BarnBaseTest {
     protected RequestSpecification newRequestSpec = RequestSpecBuilder.getRequestSpec();
 
     @BeforeMethod
     public void setTestLevelRequestSpec(Object[] params){
-        if (((Map<String,String>) params[0]).get("setAuth").equalsIgnoreCase("Y")) {
-            if (((Map<String,String>) params[0]).get("authType").equalsIgnoreCase("oauth2CC")) {
-                newRequestSpec = newRequestSpec.auth().oauth2(AuthorizationUtils.getAuthToken(((Map<String,String>) params[0]).get("authParams").split(";")));
-            }
-        }
-        if (((Map<String,String>) params[0]).get("setPathParams").equalsIgnoreCase("Y")) {
-           newRequestSpec = RequestSpecBuilder.getPathParams(newRequestSpec,((Map<String,String>) params[0]).get("pathParams").split(";"));
-        }
+        newRequestSpec = getAuth(newRequestSpec, (Map<String, String>) params[0]);
+        newRequestSpec = getPathParams(newRequestSpec, (Map<String, String>) params[0]);
     }
 }
