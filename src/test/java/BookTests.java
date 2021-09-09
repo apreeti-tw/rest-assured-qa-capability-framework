2ef98197221738080b19746f9f7086ac548f6747
apreeti-tw <apreeti@thoughtworks.com>
2021-09-09 23:33:12 +0530

    Validate size of the books list in response

--- src/test/java/BookTests.java
+++ src/test/java/BookTests.java
@@ -1,4 +1,6 @@
 import base.BookBaseTest;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import builders.BookBuilder;
 import utils.DataProviderUtils;
@@ -11,10 +13,15 @@ import static enums.EndPoints.ADD_BOOKS_LIST;
 public class BookTests extends BookBaseTest {
     @Test(dataProvider = "DataContainer", dataProviderClass = DataProviderUtils.class)
     public void testAddListOfBooks(Map<String,String> book) throws IOException {
-        requestSpecification
+        Response response = requestSpecification
+                .log()
+                .all()
                 .body(new BookBuilder(book).setBookData())
                 .post(ADD_BOOKS_LIST.getEndPoint())
-                .then()
-                .statusCode(Integer.parseInt(book.get("expected")));
+                        .then().log().all()
+                        .extract().response();
+
+        Assert.assertEquals(response.statusCode(), Integer.parseInt(book.get("expected")));
+        Assert.assertEquals(response.jsonPath().getInt("books.size()"), 4);
     }
 }
