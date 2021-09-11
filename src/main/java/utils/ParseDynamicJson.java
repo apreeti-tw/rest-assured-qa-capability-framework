460beef091eb801726f1b31787545698f6d4470f
Ayush Agrawal <ayushagrawal@Ayushs-MacBook-Air.local>
2021-09-11 22:26:25 +0530

    Ayush|Modified Parse Dynamic FIle

--- src/main/java/utils/ParseDynamicJson.java
+++ src/main/java/utils/ParseDynamicJson.java
@@ -11,9 +11,9 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class ParseDynamicJson {
+    public static Object obj=null;
 
     public static Object parseObject(JSONObject json, String key) throws JSONException {
-
         System.out.println("Dynamic key value is " + json.get(key));
         return json.get(key);
     }
@@ -22,7 +22,6 @@ public class ParseDynamicJson {
     public static Object getKey(JSONObject json, String key) throws JSONException {
 
         boolean exists = json.has(key);
-        Object obj = null;
         Iterator<?> keys;
         String nextKeys;
         if (!exists) {
@@ -31,7 +30,7 @@ public class ParseDynamicJson {
                 nextKeys = (String) keys.next();
                 try {
                     if (json.get(nextKeys) instanceof JSONObject) {
-                        getKey(json.getJSONObject(nextKeys), key);
+                       getKey(json.getJSONObject(nextKeys), key);
                     } else if (json.get(nextKeys) instanceof JSONArray) {
                         JSONArray jsonArray = json.getJSONArray(nextKeys);
                         for (int i = 0; i < jsonArray.length(); i++) {
@@ -47,7 +46,7 @@ public class ParseDynamicJson {
                 }
             }
         } else
-            obj = parseObject(json, key);
+           obj= parseObject(json, key);
         return obj;
     }
 
@@ -59,11 +58,11 @@ public class ParseDynamicJson {
             for (int i = 0; i < inputJsonArray.length(); i++) {
                 String jsonArrayString = inputJsonArray.get(i).toString();
                 JSONObject innerJson = new JSONObject(jsonArrayString);
-                obj = getKey(innerJson, key);
+                obj=getKey(innerJson, key);
             }
         } else {
             JSONObject inputJson = new JSONObject(JsonResponse);
-            obj = getKey(inputJson, key);
+               obj=getKey(inputJson, key);
         }
         return obj;
     }
@@ -77,7 +76,8 @@ public class ParseDynamicJson {
             e.printStackTrace();
         }
         System.out.println("File content is "+ content);
-        ParseDynamicJson.ValidateJson(content, "purchaseAmount");
+      Object obj= ParseDynamicJson.ValidateJson(content, "purchaseAmount");
+        System.out.println("Parse Json is "+obj);
     }
 
 
