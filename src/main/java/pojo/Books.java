bd0f395431a4bf85bf07634e558cd347f15f9a24
apreeti-tw <apreeti@thoughtworks.com>
2021-09-12 20:08:05 +0530

    Do dynamic assertion on response data

--- src/main/java/pojo/Books.java
+++ src/main/java/pojo/Books.java
@@ -5,14 +5,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Book {
-    private String userId;
-    private List<Isbn> collectionOfIsbns;
-    private List<Books> books;
+public class Books {
+    private String isbn;
 }
--- src/test/java/BookTests.java
+++ src/test/java/BookTests.java
@@ -3,21 +3,40 @@ import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import builders.BookBuilder;
+import pojo.Book;
+import pojo.Books;
+import pojo.Isbn;
 import utils.DataProviderUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import static enums.EndPoints.ADD_BOOKS_LIST;
+import static utils.DataProviderUtils.getData;
 
 public class BookTests extends BookBaseTest {
     @Test(dataProvider = "DataContainer", dataProviderClass = DataProviderUtils.class)
     public void testAddListOfBooks(Map<String,String> book) throws IOException {
-        Response response = requestSpecification
+        Book books = requestSpecification
                 .body(new BookBuilder(book).setBookData())
-                .post(ADD_BOOKS_LIST.getEndPoint());
+                .post(ADD_BOOKS_LIST.getEndPoint())
+                .then()
+                .assertThat()
+                .statusCode(Integer.parseInt(book.get("expected")))
+                .extract()
+                .response()
+                .as(Book.class);
 
-        Assert.assertEquals(response.statusCode(), Integer.parseInt(book.get("expected")));
-        Assert.assertEquals(response.jsonPath().getInt("books.size()"), 4);
+        List<String> expectedList = new ArrayList();
+        books.getBooks().parallelStream().forEach(bookId-> expectedList.add(bookId.getIsbn()));
+
+        List<String> actualList = new ArrayList();
+        Arrays.stream(getData("BookData_Isbn", book.get("testname"))).forEach(isbn ->
+                actualList.add(((Map<String,String>)isbn).get("isbn")));
+
+        Assert.assertEquals(expectedList, actualList);
     }
 }
