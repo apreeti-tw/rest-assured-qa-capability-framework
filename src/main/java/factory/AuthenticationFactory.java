e9b7f4fb9491bb0c633a5c08321c773cf3d28193
apreeti-tw <apreeti@thoughtworks.com>
2021-09-14 00:02:44 +0530

    Revert "Revert "Update code as per inbuilt RequestSpecBuilder""
    This reverts commit 734d75737d8050c420f163cb2b07d09b07e9d6e2.

--- src/main/java/factory/AuthenticationFactory.java
+++ src/main/java/factory/AuthenticationFactory.java
@@ -1,10 +1,12 @@
 package factory;
 
+import io.restassured.authentication.*;
 import io.restassured.specification.RequestSpecification;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import static utils.AuthorizationUtils.getAuthToken;
 import static utils.EncryptionUtils.decodeBase64;
@@ -12,21 +14,23 @@ import static utils.EncryptionUtils.decodeBase64;
 public final class AuthenticationFactory {
     private AuthenticationFactory(){}
 
-    private static BiFunction<RequestSpecification, Map<String, String>, RequestSpecification> oAuth2CC = (reqSpec, parameters) -> reqSpec.auth().oauth2(getAuthToken(parameters.get("base_url"), parameters.get("authParams").split(";")));
-
-    private static BiFunction<RequestSpecification, Map<String, String>, RequestSpecification> basic = (reqSpec, parameters) -> {
+    private static Function<Map<String, String>, AuthenticationScheme> basic = (parameters) -> {
         String[] credentials = parameters.get("authParams").split(":");
-        return reqSpec.auth().preemptive().basic(decodeBase64(credentials[0]), decodeBase64(credentials[1]));
+
+        PreemptiveBasicAuthScheme basicAuthScheme = new PreemptiveBasicAuthScheme();
+        basicAuthScheme.setUserName(decodeBase64(credentials[0]));
+        basicAuthScheme.setPassword(decodeBase64(credentials[1]));
+
+        return basicAuthScheme;
     };
 
-    private static Map<String, BiFunction<RequestSpecification, Map<String, String>, RequestSpecification>> authenticationMap = new HashMap();
+    private static Map<String, Function<Map<String, String>, AuthenticationScheme>> authenticationMap = new HashMap();
 
     static {
-        authenticationMap.put("oAuth2CC", oAuth2CC);
         authenticationMap.put("Basic", basic);
     }
 
-    public static RequestSpecification executeAuth(RequestSpecification reqSpec, Map<String, String> parameters){
-        return authenticationMap.get(parameters.get("authType")).apply(reqSpec, parameters);
+    public static AuthenticationScheme executeAuth(Map<String, String> parameters){
+        return authenticationMap.get(parameters.get("authType")).apply(parameters);
     }
 }
